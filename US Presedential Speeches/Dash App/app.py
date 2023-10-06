74745b9c09db951206bb38bf9b57409710f2c65a
Kasheme <kasheme.walton@outlook.com>
2023-10-06 20:48:17 +0100

    Speech Analysis Updates

--- US Presedential Speeches/Dash App/app.py
+++ US Presedential Speeches/Dash App/app.py
@@ -12,7 +12,7 @@ app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.VAPOR
 app.layout = html.Div(
     [
         html.Div("U.S. Presedential Speeches on Middle Eastern Conflict 2003-2021", style={'fontSize':40, 'textAlign':'center'}),
-        html.Div("Topic Modelling and Sentiment Analysis", style={'fontSize':30, 'textAlign':'center'}),
+        html.Div("Topic Modelling Analysis", style={'fontSize':30, 'textAlign':'center'}),
         html.Div([
             dcc.Link(page['name'] + " | ", href=page['path'])
             for page in dash.page_registry.values()
